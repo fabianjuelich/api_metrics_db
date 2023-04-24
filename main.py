fc71bb8926c7e389f3f7a43fc37e675e60950d2e
Fabian <63878623+fabianjuelich@users.noreply.github.com>
2023-04-24 17:39:17 +0200

    Add docstrings for commonly used functions

--- main.py
+++ main.py
@@ -3,11 +3,20 @@ from src.function import Function
 from src.fiscal import Fiscal
 from src.indicator import Indicator as Ind, indicators as inds
 import requests
-import json
-from datetime import datetime as dt
-from datetime import timedelta as td
 
-def alpha_vantage(function, symbol, interval=None, apikey=cred.apikey) -> json:
+def alpha_vantage(function, symbol, interval=None, apikey=cred.apikey) -> dict:
+    """
+    Calls Alpha Vantage API and returns a JSON object containing the result of the request.
+    Args:
+        function (Function): An enum value representing the function to be called from the Alpha Vantage API.
+        symbol (str): The stock symbol for which the function is to be called.
+        interval (int, optional): The time interval (in minutes) for which to get the time series data (default None).
+        apikey (str, optional): The API key to be used for authentication (default cred.apikey).
+    Returns:
+        A JSON object containing the result of the API request.
+    Raises:
+        Exception: If the API returns an error.
+    """
     base = f'https://www.alphavantage.co/query?function={function.upper()}&symbol={symbol}'
     inter = f'&interval={interval}min'
     key = f'&apikey={apikey}'
@@ -19,6 +28,19 @@ def alpha_vantage(function, symbol, interval=None, apikey=cred.apikey) -> json:
     return data
 
 def get_latest_report(symbol, function: Function, fiscal: Fiscal=None, fiscalDateEnding=None, index=None):
+    """
+    Gets the latest report of a given function for a given symbol and fiscal year ending.
+    Args:
+        symbol (str): The stock symbol for which to get the report.
+        function (Function): An enum value representing the function to be called from the Alpha Vantage API.
+        fiscal (Fiscal, optional): An enum value representing the fiscal year for which to get the report (default None).
+        fiscalDateEnding (str, optional): A string representing the fiscal year ending for which to get the report (default None).
+        index (int, optional): The index of the report to be returned (default None).
+    Returns:
+        The latest report (, the index in case of periodically reports).
+    Args:
+        Exception: If an error occurs while getting the report or the fiscal date ending is not found.
+    """
     try:
         reports = alpha_vantage(function, symbol)
     except Exception as e:
@@ -48,7 +70,18 @@ def get_latest_report(symbol, function: Function, fiscal: Fiscal=None, fiscalDat
     else:
         return reports
 
-def get_latest_series(symbol, function: Function=Function.TIME_SERIES_INTRADAY, interval=1) -> json:
+def get_latest_series(symbol, function: Function=Function.TIME_SERIES_INTRADAY, interval=1) -> dict:
+    """
+    Gets the latest time series data for a given symbol and interval.
+    Args:
+        symbol (str): The stock symbol for which to get the time series data.
+        function (Function, optional): An enum value representing the function to be called from the Alpha Vantage API (default Function.TIME_SERIES_INTRADAY).
+        interval (int, optional): The time interval (valid values: 1, 5, 15, 30, 60 minutes) for which to get the time series data (default 1).
+    Returns:
+        The latest time series data for the given symbol and interval.
+    Raises:
+        Exception: If an error occurs while getting the time series data.
+    """
     try:
         lastTimeSeries = alpha_vantage(function, symbol, interval=interval)[f'Time Series ({interval}min)']
     except Exception as e:
@@ -56,6 +89,13 @@ def get_latest_series(symbol, function: Function=Function.TIME_SERIES_INTRADAY, 
     return lastTimeSeries[list(lastTimeSeries)[0]]
 
 def get_currency(report) -> str:
+    """
+    Gets the currency of the given report.
+    Args:
+        report (dict): A dictionary representing the report for which to get the currency.
+    Returns:
+        A string representing the currency of the given report.
+    """
     try:
         return report['reportedCurrency']
     except:
@@ -97,7 +137,7 @@ def gross_profit(symbol, fiscal: Fiscal=None, fiscalDateEnding=None):
         )
     costOfRev = int(costOfRev_rep[inds[Ind.GROSS_PROFIT][1].key])
 
-    gp = totRev - costOfRev, get_currency(totRev_rep) if get_currency(totRev_rep) == get_currency(costOfRev_rep) else None
+    gp = totRev-costOfRev, get_currency(totRev_rep) if get_currency(totRev_rep) == get_currency(costOfRev_rep) else None
 
     # given
     rep, _ = get_latest_report(symbol, Function.INCOME_STATEMENT, fiscal, fiscalDateEnding)
@@ -188,7 +228,7 @@ def market_capitalization(symbol):
         inds[Ind.MARKET_CAPITALIZATION][1].function)
     num = int(num_rep[inds[Ind.MARKET_CAPITALIZATION][1].key])
 
-    markCap = round(close * num, 4)
+    markCap = round(close*num, 4)
 
     # given
     rep = get_latest_report(
