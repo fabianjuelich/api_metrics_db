a9c71c26c7c323165e981716259d71779c77dde9
Fabian Jülich <63878623+fabianjuelich@users.noreply.github.com>
2023-09-02 00:42:44 +0200

    Setup RPC Client

---
+++ compose/Cron/cronjob.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from xmlrpc.client import ServerProxy
+from elasticsearch import Elasticsearch
+from datetime import datetime
+
+# setup
+es = Elasticsearch("http://elasticsearch:9200")
+ser = ServerProxy("http://app:2900")
+
+# example call
+doc = ser.test()
+# example storing
+res = es.index(
+    index="lazy-investor",
+    id ="my_id",
+    document=doc
+)
