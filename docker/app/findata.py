52c353dfc2d88b9ac2c35e85d53ab2ef9a5dac01
Fabian Jülich <63878623+fabianjuelich@users.noreply.github.com>
2023-09-16 18:34:33 +0200

    Add necessary calculations for Leeway

--- docker/app/findata.py
+++ docker/app/findata.py
@@ -28,6 +28,14 @@ class METRICS(StrEnum):
     PRICE_TO_BOOK_VALUE = auto()
     PRICE_TO_CASHFLOW = auto()
 
+# save typing #
+
+def get_value_by_list(j: dict, l: list):
+    value = j
+    for key in l:
+        value = value[key]
+    return float(value)
+
 # parent class #
 
 class Findata():
@@ -41,10 +49,10 @@ class Findata():
 # child classes #
 
 class AlphaVantage(Findata):
-    pass
+    pass    # include previous class
 
 class FinancialModelingPrep(Findata):
-    pass
+    pass    # ToDo
 
 class Leeway(Findata):
 
@@ -52,22 +60,29 @@ class Leeway(Findata):
             ('Highlights', 'QuarterlyRevenueGrowthYOY'),
             ('Highlights', 'GrossProfitTTM'),
             ('Highlights', 'ReturnOnEquityTTM'),
-            {'one': (), 'two': ()},
-            {'one': (), 'two': ()},
+            {
+             'SHAREHOLDERS_EQUITY':('Financials', 'Balance_Sheet', 'quarterly', 0, 'totalStockholderEquity'),
+             'LIABILITIES': ('Financials', 'Balance_Sheet', 'quarterly', 0, 'totalLiab')
+             },
+            {
+             'DEBT': ('Financials', 'Balance_Sheet', 'quarterly', 0, 'longTermDebt'),
+             'SHAREHOLDERS_EQUITY': ('Financials', 'Balance_Sheet', 'quarterly', 0, 'totalStockholderEquity')
+            },
             ('Highlights', 'MarketCapitalization'),
             ('Valuation', 'EnterpriseValue'),
             ('Valuation', 'EnterpriseValueRevenue'),
             ('Valuation', 'EnterpriseValueEbitda'),
             ('Highlights', 'PERatio'),
             ('Valuation', 'PriceBookMRQ'),
-            {'one': (), 'two': ()}
+            {
+             'MARKET_CAPITALIZATION': ('Highlights', 'MarketCapitalization'),
+             'CASHFLOW': ('Financials', 'Cash_Flow', 'quarterly', 0, 'totalCashFromOperatingActivities')
+            }
     ]))
 
     def __init__(self):
         self.url = 'https://api.leeway.tech/api/v1/public/'
         self.data = None
-
-        # print(json.dumps(self.__leeway, indent=4))
 
     def __get(self, symbol, exchange, typ='fundamentals', token=tokens.leeway, lang='english', fmt='JSON'): # ToDo: rename later to get()
         url = self.url + f'{typ}/{symbol}.{exchange}'
@@ -94,14 +109,22 @@ class Leeway(Findata):
         result = {}
         for metric in METRICS:
             match(metric):
+                # not given
                 case METRICS.EQUITY_RATIO:
-                    pass
+                    shareholders_equity = get_value_by_list(self.data, self.__leeway[metric]['SHAREHOLDERS_EQUITY'])
+                    total_assets = get_value_by_list(self.data, self.__leeway[metric]['LIABILITIES'])
+                    result[metric.name] = shareholders_equity / (total_assets + shareholders_equity)
                 case METRICS.GEARING_RATIO:
-                    pass
+                    shareholders_equity = get_value_by_list(self.data, self.__leeway[metric]['SHAREHOLDERS_EQUITY'])
+                    debt = get_value_by_list(self.data, self.__leeway[metric]['DEBT'])
+                    result[metric.name] = debt / shareholders_equity
                 case METRICS.PRICE_TO_CASHFLOW:
-                    pass
+                    market_capitalization = get_value_by_list(self.data, self.__leeway[metric]['MARKET_CAPITALIZATION'])
+                    cashflow = get_value_by_list(self.data, self.__leeway[metric]['CASHFLOW'])
+                    result[metric.name] = market_capitalization / cashflow
+                # given
                 case _:
-                    result[metric.name] = self.data[self.__leeway[metric][0]][self.__leeway[metric][1]]
+                    result[metric.name] = get_value_by_list(self.data, self.__leeway[metric])
         return(result)
 
     def sector(self):
