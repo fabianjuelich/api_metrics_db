fc9dfc330daea13b365c71063e0a4ce3f17c4114
Fabian Jülich <63878623+fabianjuelich@users.noreply.github.com>
2023-10-25 17:02:56 +0200

    Update help-procedure

--- docker/app/interface.py
+++ docker/app/interface.py
@@ -9,16 +9,31 @@ import json
 
 class RPC_Server:
     def metrics(self, sort: int, symbols: str, country_codes: str, api: int):
-        api = Api(api)
-        api = findata.AlphaVantage() if api is Api.ALPHA_VANTAGE else \
-            findata.Leeway() if api is Api.LEEWAY else \
-            findata.FinancialModelingPrep() if api is Api.FINANCIAL_MODELING_PREP \
-            else None
-        return json.dumps(app.document(Sort(sort), symbols, country_codes, api))
+        try:
+            api = Api(api)
+            api = findata.AlphaVantage() if api is Api.ALPHA_VANTAGE else \
+                findata.Leeway() if api is Api.LEEWAY else \
+                findata.FinancialModelingPrep() if api is Api.FINANCIAL_MODELING_PREP \
+                else None
+            return json.dumps(app.document(Sort(sort), symbols, country_codes, api))
+        except Exception as e:
+            print(e)
+            help()
     
     def help(self):
-        return '\t\tsymbols\tcountry_codes\nSORT.INDEX:\t1..*\t0\nSORT.MARKET:\t0\t1..*\nSORT.STOCK:\t1..*\t1'
-    
+        return f'''
+lazy-investor - Help (server.help())
+
+Cardinality:
+sort        symbols country_codes api
+SORT.INDEX     1..*             0   1
+SORT.MARKET       0          1..*   1
+SORT.STOCK     1..*             1   1
+
+Values:
+{[{c.__name__:dict([(o.value, o.name) for o in c])} for c in (Sort, Api)]}
+'''
+
 # server setup #
     
 rpc_server = SimpleXMLRPCServer(("0.0.0.0", 2900), allow_none=True)
